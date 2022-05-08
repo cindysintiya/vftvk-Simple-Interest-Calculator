c4d095992798fb2e249e0f0cc6371c3ed39bd717
Cindy <211110347@students.mikroskil.ac.id>
2022-05-08 14:14:18 +0000

    changes made from cindysintiya

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate").step = "0.25";
+    document.getElementById("rate_val").innerText=rateval;
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principals = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    if (principals="" || principals<=0) {
+        window.alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    } 
+    else {
+        var interest = principals * years * rate / 100;
+        var year = new Date().getFullYear()+parseInt(years);
     
+        document.getElementById("result").innerHTML= "If you deposit <mark>" + principals + "</mark>,<br>at an interest rate of <mark>" + rate + "%</mark><br>You will receive an amount of <mark>" + interest + "</mark>,<br>in the year <mark>" + year + "</mark><br>";
+    }
 }
-        
